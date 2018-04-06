edf129dc85e826b79a8ab7bab8664c7a18c2ae7d
Terrence Wong <wongterrencew@gmail.com>
2018-04-06 14:30:52 +0200

    remove unused div

--- app/js/App.js
+++ app/js/App.js
@@ -192,7 +192,6 @@ export class App extends React.Component {
                 />
               ))}
             </MyMapComponent>
-            <div id='map' />
           </div>
 
           <div className='clear-both' />
