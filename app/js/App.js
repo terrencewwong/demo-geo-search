ace331715669879724408b61b2993c362790a572
Terrence Wong <wongterrencew@gmail.com>
2018-04-06 18:15:16 +0200

    make it pretty

--- app/js/App.js
+++ app/js/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import capitalize from 'lodash.capitalize'
 import algoliasearch from 'algoliasearch'
 import algoliasearchHelper from 'algoliasearch-helper'
+import styled, { injectGlobal } from 'styled-components'
+import styledMap from 'styled-map'
 import {
   withScriptjs,
   withGoogleMap,
@@ -10,9 +12,112 @@ import {
   Circle
 } from 'react-google-maps'
 
-import { Header } from './Header'
-import { Footer } from './Footer'
+injectGlobal`
+  html, body {
+    margin: 0;
+  }
 
+  * {
+    -webkit-font-smoothing: antialiased;
+  }
+`
+
+const grey = 'rgb(145, 165, 174)'
+const darkerGrey = 'rgb(8, 55, 73)'
+
+const FullScreen = styled.div`
+  width: 100vw;
+  height: 100vh;
+`
+
+const Layout = styled.div`
+  display: flex;
+  height: 100%;
+`
+
+const MainContent = styled.div`
+  flex: 1;
+  height: 100%;
+`
+
+const Panel = styled.div`
+  width: 384px;
+`
+
+const Icon = styled.div`
+  font-size: 24px;
+  line-height: 24px;
+  padding-top: 6px;
+  margin-bottom: 6px;
+  ${props => (props.color ? `color: ${props.color}` : '')};
+
+  ${styledMap('name', {
+    close: `
+      &::before {
+        content: '\\2573';
+      }
+    `,
+    magnifyingGlass: `
+      &::before {
+        content: '\\1F50D';
+      }
+    `
+  })};
+`
+
+const _SearchHeader = styled.div`
+  align-items: center;
+  color: ${grey};
+  display: flex;
+  font-size: 20px;
+  height: 64px;
+  justify-content: center;
+  line-height: 20px;
+  position: relative;
+
+  .absolute {
+    position: absolute;
+    top: 25%;
+    right: 24px;
+  }
+`
+const SearchHeader = ({ onClick }) => (
+  <_SearchHeader>
+    Trouver un docteur
+    <div className='absolute'>
+      <Icon color={darkerGrey} name='close' onClick={onClick} />
+    </div>
+  </_SearchHeader>
+)
+
+const DoctorSearchWrapper = styled.div`
+  position: relative;
+
+  .absolute {
+    position: absolute;
+    top: 25%;
+    left: 24px;
+  }
+`
+
+const DoctorSearchInput = styled.input`
+  border: none;
+  box-shadow: 0px 1px 1px #eee, 0px -1px 1px #eee;
+  font-size: 16px;
+  height: 64px;
+  outline: none;
+  padding: 0 24px 0 64px;
+  width: 100%;
+`
+
+const DoctorSearch = props => (
+  <DoctorSearchWrapper>
+    <div className='absolute'>
+      <Icon name='magnifyingGlass' />
+    </div>
+    <DoctorSearchInput {...props} />
+  </DoctorSearchWrapper>
+)
 // const PARIS_COORDINATES = {
 //   lat: 48.8566,
 //   lng: 2.3522
@@ -58,8 +163,7 @@ const _MyMapComponent = withScriptjs(
         mapTypeControl: false,
         zoom: 14,
         minZoom: 10,
-        maxZoom: 20,
-        styles: [{ stylers: [{ hue: '#3596D2' }] }]
+        maxZoom: 20
       }}
     >
       {props.children}
@@ -70,9 +174,9 @@ const _MyMapComponent = withScriptjs(
 const MyMapComponent = props => (
   <_MyMapComponent
     googleMapURL='//maps.googleapis.com/maps/api/js?key=AIzaSyBnDR4e5_qobPG6Vn_zjhc1vyOIooChZt8'
-    loadingElement={<div style={{ height: `100%` }} />}
-    containerElement={<div style={{ height: '614px', width: `720px` }} />}
-    mapElement={<div style={{ height: `100%` }} />}
+    loadingElement={<div style={{ height: '100%' }} />}
+    containerElement={<div style={{ height: '100%' }} />}
+    mapElement={<div style={{ height: '100%' }} />}
     {...props}
   />
 )
@@ -141,33 +245,9 @@ export class App extends React.Component {
 
   render () {
     return (
-      <React.Fragment>
-        <Header />
-
-        <section className='map_section'>
-          <div className='left-column'>
-            <input
-              id='search-input'
-              type='text'
-              autoComplete='off'
-              spellCheck='false'
-              autoCorrect='off'
-              placeholder='Search by name, city, airport code...'
-              onChange={this.handleChange}
-            />
-            {this.state.hits.length === 0 ? (
-              <div id='no-results-message'>
-                <p>We didn't find any airports in this location.</p>
-              </div>
-            ) : (
-              <div id='hits'>
-                {this.state.hits
-                  .slice(0, 20)
-                  .map(hit => <Hit key={hit.objectID} hit={hit} />)}
-              </div>
-            )}
-          </div>
-          <div className='right-column'>
+      <FullScreen>
+        <Layout>
+          <MainContent>
             <MyMapComponent>
               <CircleWithOnMountCallback
                 onMount={boundingBox => this.registerBoundingBox(boundingBox)}
@@ -195,13 +275,31 @@ export class App extends React.Component {
                 />
               ))}
             </MyMapComponent>
-          </div>
-
-          <div className='clear-both' />
-        </section>
-
-        <Footer />
-      </React.Fragment>
+          </MainContent>
+          <Panel>
+            <SearchHeader />
+            <DoctorSearch
+              type='text'
+              autoComplete='off'
+              spellCheck='false'
+              autoCorrect='off'
+              placeholder='Nom / spécialtié (Généraliste, dentiste...)'
+              onChange={this.handleChange}
+            />
+            {this.state.hits.length === 0 ? (
+              <div id='no-results-message'>
+                <p>We didn't find any airports in this location.</p>
+              </div>
+            ) : (
+              <div id='hits'>
+                {this.state.hits
+                  .slice(0, 20)
+                  .map(hit => <Hit key={hit.objectID} hit={hit} />)}
+              </div>
+            )}
+          </Panel>
+        </Layout>
+      </FullScreen>
     )
   }
 }
